278dd6a139da44d9050eb98b713e08a496b2d80a
nailt <nailtarcan@gmail.com>
2019-07-06 16:46:28 -0400

    Made the site nicer-looking, using session variables to use it as a one-page app.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, session
 import sys
 import io
 import os
@@ -7,15 +7,25 @@ import utils.ImagifAlgorithms as ImagifAlgorithms
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+exportedGif = False
+
 
 UTILS_FOLDER = os.path.dirname(os.path.realpath(__file__)) + "\\utils"
 app.config['UTILS_FOLDER'] = UTILS_FOLDER
-
-algo = ImagifAlgorithms.Imagif(UTILS_FOLDER)
+app.config['IMAGE_OUTPUT_FOLDER'] = os.path.dirname(os.path.realpath(__file__)) + "\\static\\images"
+app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsw5'
+algo = ImagifAlgorithms.Imagif(UTILS_FOLDER, app.config['IMAGE_OUTPUT_FOLDER'])
 
 @app.route("/")
 def serveIndex():
-    return render_template("index.html")
+    print("convertedGif" in session, file=sys.stderr)
+    if "convertedGif" in session:
+        print(session.get("convertedGif"), file=sys.stderr)
+        return render_template("index.html", convertedGif=session.get("convertedGif"))
+    else:
+        return render_template("index.html", convertedGif=False)
+   
+    
 
 @app.route("/OnImageRecieved", methods=['POST'])
 def handleImage():
@@ -28,7 +38,8 @@ def handleImage():
     print(algo.get_target_dir(), file=sys.stderr)
     algo.use_noise_switch(filename, "output.gif")
     os.remove(os.path.join(app.config['UTILS_FOLDER'], filename))
-    return "Success."    
+    session["convertedGif"]= True
+    return redirect(url_for("serveIndex"))   
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- static/js/site.js
+++ static/js/site.js
@@ -1,5 +1,21 @@
+var reader = new FileReader();
+document.getElementById("file-input").addEventListener("input", function(){
+    console.log("Callback here?");
+    if(this.files && this.files[0])
+    {
+        reader.onload = function(e){
+            document.getElementById("imageField").setAttribute('src', e.target.result);
+            document.getElementById("imageField").style.visibility = "visible";
+            document.getElementById("imageFieldEmpty").style.visibility = "hidden";
+        }
+
+        
+        reader.readAsDataURL(this.files[0]);
+    }
+});
 document.getElementById('upload-form').addEventListener("submit", function(){
     console.log("Form submitted");
     console.log("Image: " + document.getElementById('file-input').files[0])
 }, false);
+
 
